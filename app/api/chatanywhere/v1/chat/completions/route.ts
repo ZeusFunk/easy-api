2320a1f531401fbe5969747c165e0e7a0416e9ef
ZeusFunk <2604243189zjx@gmail.com>
2023-09-13 07:57:45 +0800

    完成聊天部分的转发

---
+++ app/api/chatanywhere/v1/chat/completions/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const apiKey = process.env.OPENAI_API_KEY;
+  const url = process.env.OPENAI_PROXY_URL + "/v1/chat/completions";
+  const body = await req.json();
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + apiKey,
+      },
+      body: JSON.stringify(body),
+    });
+    return response;
+  } catch (error) {
+    return new Response(error as string, { status: 500 });
+  }
+}
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Chat-Api-Provider",
+  description: "用于各种聊天大模型api的转发，以及类openai似流式处理",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
