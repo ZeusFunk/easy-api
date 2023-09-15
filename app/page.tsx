5febb166353777037950ac6f5194c223de594bfe
ZeusFunk <2604243189zjx@gmail.com>
2023-09-15 17:42:39 +0800

    改了一丢丢主页、和readme

--- app/page.tsx
+++ app/page.tsx
@@ -142,22 +142,21 @@ export default function Home() {
               不受网络限制
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              通过服务器转发API端口实现无论任何地区的网络都可以直接请求API,<br/>
+              让你无需再反复切换API的URI。
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-indigo-600 px-10 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                开始
               </a>
               <a
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
-                Learn more <span aria-hidden="true">→</span>
+                查看细节<span aria-hidden="true"> →</span>
               </a>
             </div>
           </div>
